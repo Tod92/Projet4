65f8327d923569f72c538ae47b22210fc412710f
Thomas Deruere <t.deruere@gmail.com>
2023-01-18 22:16:16 +0100

    rework point d'entree chess.py

--- Code/chess.py
+++ Code/chess.py
@@ -1,8 +1,22 @@
-#from Controller import menucontroller
+"""Entry point."""
 
-#menucontroller.main()
+from models.database import Database
 
+from controllers.base import Controller
 
+from views.base import Views
+from views.console import ConsoleView
+from views.reporting import Reporting
+
+def main():
+    database = Database()
+
+    active_view = ConsoleView()
+    passive_views = (active_view, Reporting())
+    views = Views(active_view, passive_views)
+
+    app = Controller(database, views)
+    app.run()
 
 if __name__ == "__main__":
     main()
